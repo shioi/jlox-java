d4cd3cff150c271b61ca2e16360f436bd959f661
isyum <saiyum303@gmail.com>
2023-02-28 00:04:06 +0530

    added gitignore

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -1,4 +1,4 @@
-package com.craftinginterpreters;
+package com.craftinginterpreters.lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.nio.file.Files;
-import java.util.Scanner;
 import java.util.List;
 
 public class Lox {
@@ -48,7 +47,6 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        //for now, just print the tokens.
         for(Token token: tokens){
             System.out.println(token);
         }
